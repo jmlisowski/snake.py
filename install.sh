e58003dd50e0713f888f02020b42d484c399eca8
jmlisowski <54516275+jmlisowski@users.noreply.github.com>
2020-05-17 13:44:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 wget https://raw.githubusercontent.com/jmlisowski/snake.py/master/snake.py
 wget https://raw.githubusercontent.com/jmlisowski/snake.py/install/snake
-sudo mkdir /usr/lib/.snake
-sudo mv snake.py /usr/lib/.snake
+sudo mkdir /usr/lib/snake
+sudo mv snake.py /usr/lib/snake
 sudo mv snake /usr/bin
 sudo chmod +x /usr/bin/snake
 echo Type In the this command: snake
